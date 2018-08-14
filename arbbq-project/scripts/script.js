248997661a8dcf084bcc07ed9356b39a5af704e0
Jack <jack.kalish@themill.com>
2018-08-14 17:42:20 -0400

    laser beams cook the dogs!

--- arbbq-project/scripts/script.js
+++ arbbq-project/scripts/script.js
@@ -2,7 +2,7 @@
 // Available modules include (this is not a complete list):
 var Scene = require('Scene');
 // var Textures = require('Textures');
-// var Materials = require('Materials');
+var Materials = require('Materials');
 var FaceTracking = require('FaceTracking');
 var FaceGestures = require('FaceGestures');
 var Animation = require('Animation');
@@ -10,14 +10,7 @@ var Diagnostics = require('Diagnostics');
 
 // var Reactive = require('Reactive');
 
-var hotdog = Scene.root.find('fbxHotdog');
-
-var hotdogDriver = Animation.timeDriver({durationMilliseconds: 500, loopCount:Infinity});
-var hotdogSampler = Animation.samplers.easeInSine(55,-10);
-var hotdogAnim = Animation.animate(hotdogDriver, hotdogSampler);
-hotdog.transform.z = hotdogAnim;
-
-
+//FACETRACKER stuff
 var face = FaceTracking.face(0);
 
 FaceGestures.hasMouthOpen(face).monitor().subscribe(function(changedValue) {
@@ -26,9 +19,45 @@ FaceGestures.hasMouthOpen(face).monitor().subscribe(function(changedValue) {
 		hotdogDriver.reset();
 		hotdogDriver.start();
 		hotdog.hidden = false;
+
+		leftLaser.hidden = false;
+		rightLaser.hidden = false;
+		laserDriver.reset();
+		laserDriver.start();
 	} else {
 		Diagnostics.log('Mouth closed!');
 		hotdogDriver.stop();
 		hotdog.hidden = true;
+
+		laserDriver.start();
+		leftLaser.hidden = true;
+		rightLaser.hidden = true;
 	}
 });
+
+
+var len = 1500;
+//HOTDOG!
+var hotdog = Scene.root.find('fbxHotdog');
+var hotdogDriver = Animation.timeDriver({durationMilliseconds: len, loopCount:Infinity});
+var hotdogSampler = Animation.samplers.easeInSine(55,-10);
+var hotdogAnim = Animation.animate(hotdogDriver, hotdogSampler);
+hotdog.transform.z = hotdogAnim;
+
+//LASERS!
+var leftLaser = Scene.root.find('beam-left');
+var rightLaser = Scene.root.find('beam-right');
+var laserDriver = Animation.timeDriver({durationMilliseconds: len, loopCount:Infinity});
+var laserSamplers = Animation.samplers.easeInSine([1,degToRad(90),degToRad(2), .028],[0,degToRad(130),degToRad(15), .001]); //opacity, xrot, yrot
+var laserAnim = Animation.animate(laserDriver, laserSamplers);
+Materials.get('laser-mtl').opacity = laserAnim.get(0);
+leftLaser.transform.rotationX = laserAnim.get(1);
+rightLaser.transform.rotationX = laserAnim.get(1);
+leftLaser.transform.rotationY = laserAnim.get(2);
+rightLaser.transform.rotationY = laserAnim.get(2).neg();
+leftLaser.transform.scaleY = rightLaser.transform.scaleY = laserAnim.get(3);
+
+
+//HELPERS
+function degToRad(d){return d*0.0174533;}
+
